e46b334ba683ec5812bbd00edffbfa1d7a4403b8
Raja Santhanam <santhanam.raja@gmail.com>
2012-01-02 17:30:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendsList::Application.config.secret_token = 'e7be52e3fd4228a7d1367a2124871f92bad2b588daa319d085d82dfdeee7b915cef852176f4c18b07a76f8800ea7ac7bdf850d8606a666e17fec8cf61b300e34'
